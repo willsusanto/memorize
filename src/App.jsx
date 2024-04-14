570d5dc47c2fdeaefdc8450db904fc37128e6a1a
William <william.susanto@binus.edu>
2024-04-14 16:35:59 +0700

    Initial concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
   return (
     <>
-      <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
-        <h1 className='font-bold'>Testing</h1>
+      <div className="container mx-auto flex justify-center h-screen p-10">
+      <section className="px-6 grid grid-cols-3 gap-6 w-full">
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+
+        <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+          <h1 className="font-bold">Testing</h1>
+        </div>
+      </section>
       </div>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
