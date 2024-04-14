00e84908b8fa2f502b252d074819dacb7aa13598
William <william.susanto@binus.edu>
2024-04-14 16:26:48 +0700

    Configure tailwind content scan

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 function App() {
   return (
     <>
+      <div className="bg-red-300 p-6 rounded-lg border-r-fuchsia-800">
+        <h1 className='font-bold'>Testing</h1>
+      </div>
     </>
   )
 }
