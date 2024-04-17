98550ddc3bea949fac9fdfb1d64c682d181ffbc1
William Susanto <william.susanto@binus.edu>
2024-04-17 15:35:48 +0700

    Add button concept for flipping back the cards

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Card from "../components/Card";
 import cardsData from "../data/data.json";
 
 const GamePage = () => {
   const [cards, setCards] = useState(cardsData);
+  const flipCard = useRef(null);
 
   useEffect(() => {
     const cardTimeoutDelay = setTimeout(() => {
@@ -13,14 +14,19 @@ const GamePage = () => {
         const [firstCard, secondCard] = openedCards;
         const matchedCard = firstCard.description === secondCard.description;
 
-        setCards(currentCards => {
-            return currentCards.map(card => {
-                if (card.id === firstCard.id || card.id === secondCard.id)
-                    return { ...card, open: false, completed: matchedCard, processingMatch: false };
+        setCards((currentCards) => {
+          return currentCards.map((card) => {
+            if (card.id === firstCard.id || card.id === secondCard.id)
+              return {
+                ...card,
+                open: false,
+                completed: matchedCard,
+                processingMatch: false,
+              };
 
-                return { ...card, processingMatch: false }
-            })
-        })
+            return { ...card, processingMatch: false };
+          });
+        });
       }
     }, 1500);
 
@@ -33,27 +39,38 @@ const GamePage = () => {
 
   const openCard = (event, id) => {
     event.preventDefault();
-    
+
     const openedCards = getOpenedCards();
 
     setCards((currentCard) =>
       currentCard.map((card) => {
         if (card.id === id) return { ...card, open: true, completed: false };
         if (openedCards.length == 1) return { ...card, processingMatch: true };
-        
+
         return card;
       })
     );
   };
 
+  const handleRotate = (rotate) => {
+    console.log(flipCard);
+    if (rotate) flipCard.current.classList.add("rotating");
+    else flipCard.current.classList.remove("rotating");
+  };
+
   return (
     <>
-    <div className="flipCardContainer">
-      <div className="flipCard">
-        <div className="front">Testing FRONT</div>
-        <div className="back">Testing BACK</div>
+    <button onClick={() => handleRotate(false)}>Rotate back</button>
+      <div className="flipCardContainer">
+        <div
+          ref={flipCard}
+          className="flipCard"
+          onClick={() => handleRotate(true)}
+        >
+          <div className="front">Testing FRONT</div>
+          <div className="back">Testing BACK</div>
+        </div>
       </div>
-    </div>
     </>
     // <div className="container mx-auto">
     //   <section className="px-6 grid grid-cols-card-size gap-3 w-full">
