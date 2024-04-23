9594694f143daf29ddab2e5f70dbd88d163daee5
William Susanto <william.susanto@binus.edu>
2024-04-23 10:16:08 +0700

    Example changing background image for matched cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,23 +9,19 @@ const Card = ({ id, description, open, matched, openCard, fileName }) => {
 
   return (
     <>
-      {matched ? (
-        <div className={`${defaultStyling} bg-black`}></div>
-      ) : (
         <div
           onClick={(e) => openCard(e, id)}
-          className={`flipCardBase relative transition duration-500 ${open && "flipping"} ${defaultStyling}`}
+          className={`flipCardBase relative transition duration-500 ${(open || matched) && "flipping"} ${defaultStyling}`}
         >
-          <div className="front bg-blue-100 flipping rounded-card-edge">
+          <div className={`front ${matched ? "bg-green-500" : "bg-blue-100"} flipping rounded-card-edge`}>
             {loading && <h1>Image loading...</h1>}
             {!loading && <img src={image} className="object-contain rounded-card-edge" />}
           </div>
-
-          <div className="back bg-card-back rounded-card-edge transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105  hover:brightness-105">
+          <div className="back bg-card-back rounded-card-edge transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:brightness-105">
             <img src={CardBack} className="object-contain rounded-card-edge"></img>
           </div>
         </div>
-      )}
+      
     </>
   );
 };
