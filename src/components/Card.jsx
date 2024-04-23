4c168191d3976696b75dc1d6f7c7afc83e6b7cdf
William Susanto <william.susanto@binus.edu>
2024-04-23 10:25:44 +0700

    Fade out cards that are completed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,25 +3,36 @@ import { useImage } from "../hooks/useImage";
 
 const Card = ({ id, description, open, matched, openCard, fileName }) => {
   const { image, loading, error } = useImage(fileName);
-  const defaultStyling = "rounded-card-edge aspect-square flex justify-center shadow-[0px_5px_16px_1px_rgba(0,0,0,0.5)]";
+  const defaultStyling =
+    "rounded-card-edge aspect-square flex justify-center shadow-[0px_5px_16px_1px_rgba(0,0,0,0.5)]";
 
   if (error) return <h1>Sorry! An error had occurred: {error.message}</h1>;
 
   return (
     <>
+      {matched ? (
+        <div
+          className={`opacity-0 transition-opacity duration-500 ${defaultStyling} `}
+        ></div>
+      ) : (
         <div
           onClick={(e) => openCard(e, id)}
-          className={`flipCardBase relative transition duration-500 ${(open || matched) && "flipping"} ${defaultStyling}`}
+          className={`flipCardBase relative transition duration-500 ${open && "flipping"} ${defaultStyling}`}
         >
-          <div className={`front ${matched ? "bg-green-500" : "bg-blue-100"} flipping rounded-card-edge`}>
+          <div className={`front flipping rounded-card-edge bg-blue-100`}>
             {loading && <h1>Image loading...</h1>}
-            {!loading && <img src={image} className="object-contain rounded-card-edge" />}
+            {!loading && (
+              <img src={image} className="rounded-card-edge object-contain" />
+            )}
           </div>
-          <div className="back bg-card-back rounded-card-edge transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:brightness-105">
-            <img src={CardBack} className="object-contain rounded-card-edge"></img>
+          <div className="back rounded-card-edge bg-card-back transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-105 hover:brightness-105">
+            <img
+              src={CardBack}
+              className="rounded-card-edge object-contain"
+            ></img>
           </div>
         </div>
-      
+      )}
     </>
   );
 };
